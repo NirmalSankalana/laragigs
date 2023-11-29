1f623b7568abb7c74c33d8a8005406afece2b17b
Nirmal Sankalana <sankalana.nirmal@gmail.com>
2023-11-29 07:38:14 +0530

    Add database migration

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,31 +2,10 @@
 
 namespace App\Models;
 
-class Listing
-{
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing 1',
-                'description' => 'But today, with GPT in the equation, we can rightfully ask this question again. We see all these fancy tweets and demos, but what does that mean for me as a developer today, right now, when I need to start a new full-stack web app? Do I really have to go through npm create vite my-new-app, and start with a blank page once again?'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing 2',
-                'description' => 'But today, with GPT in the equation, we can rightfully ask this question again. We see all these fancy tweets and demos, but what does that mean for me as a developer today, right now, when I need to start a new full-stack web app? Do I really have to go through npm create vite my-new-app, and start with a blank page once again?'
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id): array
-    {
-        $listings = self::all();
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
