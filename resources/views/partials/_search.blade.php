6a032e1b7b5be744f5622c1aa66a486619b2a920
Nirmal Sankalana <sankalana.nirmal@gmail.com>
2023-12-08 00:04:30 +0530

    add tag filtering and searching

--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -1,6 +1,8 @@
+<form action="/">
 <div class="input-group">
-    <input type="text" class="form-control" placeholder="Search" aria-label="search" aria-describedby="basic-addon2">
+    <input type="text" name="search" class="form-control" placeholder="Search" aria-label="search" aria-describedby="basic-addon2">
     <div class="input-group-append">
-      <button class="btn btn-outline-secondary" type="button">search</button>
+      <button class="btn btn-outline-secondary" type="submit">search</button>
     </div>
   </div>
+</form>
