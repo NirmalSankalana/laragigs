e96523d4cb62112a302753b853dfc98e4996cbdb
Nirmal Sankalana <sankalana.nirmal@gmail.com>
2023-12-11 13:04:07 +0530

    add image to sigle listings page

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -6,6 +6,7 @@
     <x-card>
         <div class="listing-details">
             <h2>{{$listing->title}}</h2>
+            <img src="{{$listing->logo ? asset('storage/'. $listing->logo): asset('images/img1.jpg')}}" class="card-img-top" alt="Listing Image">
             <p>{{$listing->description}}</p>
             <p>Location: {{$listing->location}}</p>
             <p>Email: {{$listing->email}}</p>
