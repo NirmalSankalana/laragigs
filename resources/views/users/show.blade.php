cc2da059f2410ab19bdf08aa9a713935aa8c7100
Nirmal Sankalana <sankalana.nirmal@gmail.com>
2023-12-16 23:13:51 +0530

    add logout link

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,8 +2,13 @@
 <div>
     @auth
     <h2>Welcome {{auth()->user()->name}}</h2> 
-    <a href="/listings/manage" class="btn btn-sm mx-1" type="submit">Manage Listings</a>
-    <a href="/listings/create" class="btn btn-sm mx-1" type="submit">Post a Job</a>
+
+    <form class="inline" method="POST" action="/logout">
+        <a href="/listings/manage" class="btn btn-sm mx-1" type="submit"><i data-feather="sliders"></i> Manage Listings</a>
+        <a href="/listings/create" class="btn btn-sm mx-1" type="submit"><i data-feather="plus-circle"></i> Post a Job</a>
+        @csrf
+        <button type="submit" class="btn btn-sm"><i data-feather="user-x"></i> Logout</button>
+    </form>
     @else
     <a href="/login" class="btn btn-sm mx-1" type="submit">Login</a>
     <a href="/register" class="btn btn-sm mx-1" type="submit">Register</a>
